2e265726eba251793d6a5b4c7b999e0935bda0f8
James <sunyjams@hotmail.com>
2017-09-11 21:54:29 +0800

    add upload fie

---
+++ NodeWeb/upload_file.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    switch (req.method) {
+        case 'GET':
+            show(req, res);
+            break;
+        case 'POST':
+            upload(req, res);
+            break;
+        default:
+            break;
+    }
+});
+
+function show(req, res) {
+    var html = ''
+            + '<form method="post" action="/" enctype="multipart/form-data">'
+            + '<p><input type="text" name="name" /></p>'
+            + '<p><input type="file" name="file" /></p>'
+            + '<p><input type="submit" value="Upload" /> </p>'
+            + '</form>';
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('COntent-Length', Buffer.byteLength(html));
+    res.end(html);
+}
+
+function upload(req, res) {
+    
+}
